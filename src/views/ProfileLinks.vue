6c7a5e80d3a2629843a8959ef042e451bf40413f
Pilkevych Oleksii <oleksii.pilkevych@nokia.com>
2019-05-15 01:04:01 +0200

    version for demo

---
+++ src/views/ProfileLinks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="profile-page">
+    <ul>
+      <li v-for="(achievement, index) of profile.achievements" :key="index">
+        {{ achievement.description }}
+        <!-- <RwvTag -->
+        <!-- :name="tag" -->
+        <!-- className="tag-default tag-pill tag-outline" -->
+        <!-- &gt;</RwvTag> -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import RwvArticleList from "@/components/ArticleList";
+
+export default {
+  name: "RwvProfileFavorited",
+  props: ['profile'],
+  components: {
+    RwvArticleList
+  },
+  computed: {
+    favorited() {
+      return this.$route.params.username;
+    }
+  }
+};
+</script>
